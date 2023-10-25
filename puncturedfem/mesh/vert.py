709771dab26639066e3638270c3193549fccc8f3
Sam Reynolds <sreyn@proton.me>
2023-10-25 11:23:58 -0700

    add arithmetic operations to Vert class

--- puncturedfem/mesh/vert.py
+++ puncturedfem/mesh/vert.py
@@ -64,12 +64,77 @@ class Vert:
         """
         Returns True if two Vertices have the same coordinates.
         """
-        if not isinstance(other, Vert):
-            raise TypeError("Cannot compare Vert to non-Vert")
-        return self.x == other.x and self.y == other.y
+        if isinstance(other, Vert):
+            return NotImplemented
+        diff = self - other
+        if isinstance(diff, Vert):
+            return diff.norm() < 1e-12
+        return False
 
-    def __hash__(self) -> int:
+    def __add__(self, other: object) -> object:
         """
-        Returns a hash of the vertex.
+        Returns a new Vert with coordinates equal to the sum of the
+        coordinates of the two Verts.
         """
-        return hash((self.x, self.y))
+        if not isinstance(other, Vert):
+            raise TypeError("Cannot add non-Vert to Vert")
+        return Vert(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: object) -> object:
+        """
+        Returns a new Vert with coordinates equal to the difference of the
+        coordinates of the two Verts.
+        """
+        if not isinstance(other, Vert):
+            raise TypeError("Cannot subtract non-Vert from Vert")
+        return Vert(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: object) -> object:
+        """
+        Returns a new Vert with coordinates equal to the product of the
+        coordinates of the two Verts, or the coordinates of the Vert multiplied
+        by the scalar.
+        """
+        if isinstance(other, (int, float)):
+            return Vert(self.x * other, self.y * other)
+        if isinstance(other, Vert):
+            return Vert(self.x * other.x, self.y * other.y)
+        raise TypeError(
+            "Multiplication against Vert must be by int, float, or Vert"
+        )
+
+    def __rmul__(self, other: object) -> object:
+        """
+        Returns a new Vert with coordinates equal to the product of the
+        coordinates of the two Verts, or the coordinates of the Vert multiplied
+        by the scalar.
+        """
+        if isinstance(other, (int, float)):
+            return Vert(self.x * other, self.y * other)
+        if isinstance(other, Vert):
+            return Vert(self.x * other.x, self.y * other.y)
+        raise TypeError(
+            "Multiplication against Vert must be by int, float, or Vert"
+        )
+
+    def __truediv__(self, other: object) -> object:
+        """
+        Returns a new Vert with coordinates equal to the quotient of the
+        coordinates of the two Verts, or the coordinates of the Vert divided by
+        the scalar.
+        """
+        if isinstance(other, (int, float)):
+            if other == 0:
+                raise ZeroDivisionError("Cannot divide by zero")
+            return Vert(self.x / other, self.y / other)
+        if isinstance(other, Vert):
+            if other.x == 0 or other.y == 0:
+                raise ZeroDivisionError("Cannot divide by zero")
+            return Vert(self.x / other.x, self.y / other.y)
+        raise TypeError("Division against Vert must be by int, float, or Vert")
+
+    def norm(self) -> float:
+        """
+        Returns the Euclidean norm of the vertex.
+        """
+        return (self.x**2 + self.y**2) ** 0.5
