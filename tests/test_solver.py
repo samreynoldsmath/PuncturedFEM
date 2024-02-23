cff5e5781c4be0cdf1d58ca0b708bff472f430b0
Sam Reynolds <sreyn@proton.me>
2024-02-22 17:22:24 -0800

    Revert "make error relative, not absolute"
    This reverts commit 3479bb7fef93cacb304b37db8cdea3e9cb354186.

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -6,12 +6,10 @@ Test the solver class.
 """
 
 import numpy as np
-from scipy.sparse import csr_matrix
-
 import puncturedfem as pf
 
 
-TOL = 1e-6
+TOL = 1e-12
 
 
 def build_solver() -> pf.Solver:
@@ -20,8 +18,7 @@ def build_solver() -> pf.Solver:
     """
 
     # set test parameters
-    n = 64
-    interp = 4
+    n = 16
     deg = 1
     a = 1.0
     c = 1.0
@@ -38,7 +35,7 @@ def build_solver() -> pf.Solver:
     T = pf.meshlib.pacman_subdiv(verbose=False)
 
     # build quadrature dictionary for parameterization
-    quad_dict = pf.get_quad_dict(n=n, interp=interp)
+    quad_dict = pf.get_quad_dict(n)
 
     # build global function space
     V = pf.GlobalFunctionSpace(T, deg, quad_dict, verbose=False)
@@ -60,23 +57,7 @@ def test_solver_deg1() -> None:
     x_exact = np.loadtxt("tests/data/glob_soln_n16.txt")
     x_error = x_exact - x_computed
 
-    x_exact_h1_sq_norm = compute_h1_square_norm(
-        x_exact, S.stiff_mat, S.mass_mat
-    )
-    x_error_h1_sq_norm = compute_h1_square_norm(
-        x_error, S.stiff_mat, S.mass_mat
-    )
+    h1_error = np.dot(S.stiff_mat @ x_error, x_error)
+    l2_error = np.dot(S.mass_mat @ x_error, x_error)
 
-    assert x_error_h1_sq_norm < TOL * x_exact_h1_sq_norm
-
-
-def compute_h1_square_norm(
-    x: np.ndarray, stiffness_mat: csr_matrix, mass_mat: csr_matrix
-) -> float:
-    """
-    Returns x^t A x + x^t B x, where A,B are the stiffness and mass matrices,
-    respectively.
-    """
-    h1 = np.dot(stiffness_mat @ x, x)
-    l2 = np.dot(mass_mat @ x, x)
-    return h1 + l2
+    assert h1_error + l2_error < TOL
