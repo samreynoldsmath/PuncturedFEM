0645839b5185184a40627164568c42c4be2f9d07
Sam Reynolds <sreyn@proton.me>
2024-03-03 15:42:29 -0800

    add clear outputs to notebook conversion script

--- devtools/convert_examples.sh
+++ devtools/convert_examples.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 # Converts all Jupyter notebooks in examples/ to Python scripts
 
+# directory containing this script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# directory containing Jupyter notebooks
 NOTEBOOK_DIR="$SCRIPT_DIR/../examples"
-jupyter nbconvert --to script --comment all \
+
+# clear outputs
+jupyter nbconvert --clear-output --inplace "$NOTEBOOK_DIR"/*.ipynb
+
+# convert notebooks to python scripts
+jupyter nbconvert --to script \
 	--output-dir="$NOTEBOOK_DIR" "$NOTEBOOK_DIR"/*.ipynb
