89c479196ee0ab98102c6a0240b83ab9c866ed28
Sam Reynolds <sreyn@proton.me>
2024-03-13 12:55:55 -0700

    add docstrings

--- puncturedfem/locfun/trace.py
+++ puncturedfem/locfun/trace.py
@@ -17,6 +17,7 @@ from ..mesh.edge import Edge
 from ..mesh.cell import MeshCell
 from .poly.poly import Polynomial
 
+# TODO: Move to a separate module
 FloatLike = Union[int, float, np.ndarray]
 Func_R2_R = Callable[[FloatLike, FloatLike], FloatLike]
 
@@ -57,7 +58,18 @@ class DirichletTrace:
     def __init__(
         self, edges: Union[MeshCell, list[Edge]], custom: bool = False
     ) -> None:
-        # TODO: add method docstrings
+        """
+        Constructor for the DirichletTrace class.
+
+        Parameters
+        ----------
+        edges : Union[MeshCell, list[Edge]]
+            The edges on which the trace is defined.
+        custom : bool, optional
+            A flag indicating whether the trace is defined
+            using custom functions. If True, the user must
+            set the values and functions manually.
+        """
         self.set_edges(edges)
         if custom:
             return
@@ -65,6 +77,14 @@ class DirichletTrace:
         # in the LocalFunctionSpace class
 
     def set_edges(self, edges: Union[MeshCell, list[Edge]]) -> None:
+        """
+        Set the edges on which the trace is defined.
+
+        Parameters
+        ----------
+        edges : Union[MeshCell, list[Edge]]
+            The edges on which the trace is defined.
+        """
         if isinstance(edges, MeshCell):
             self.edges = edges.get_edges()
         elif isinstance(edges, list):
@@ -78,6 +98,16 @@ class DirichletTrace:
     def set_trace_values_on_edge(
         self, edge_index: int, values: np.ndarray
     ) -> None:
+        """
+        Set the values of the trace on a specific edge.
+
+        Parameters
+        ----------
+        edge_index : int
+            The index of the edge on which the values are set.
+        values : np.ndarray
+            The values of the trace on the edge.
+        """
         if edge_index < 0 or edge_index >= self.num_edges:
             raise ValueError("The edge index is out of range")
         edge = self.edges[edge_index]
@@ -89,6 +119,14 @@ class DirichletTrace:
         self.values[start:end] = values
 
     def set_trace_values(self, values: Union[int, float, np.ndarray]) -> None:
+        """
+        Set the values of the trace on all edges.
+
+        Parameters
+        ----------
+        values : Union[int, float, np.ndarray]
+            The values of the trace on the edges.
+        """
         if isinstance(values, (int, float)):
             self.values = np.array([values for _ in range(self.num_pts)])
         elif isinstance(values, np.ndarray):
@@ -99,6 +137,14 @@ class DirichletTrace:
             self.values = values
 
     def set_funcs(self, funcs: Union[Func_R2_R, list[Func_R2_R]]) -> None:
+        """
+        Set the functions used to define the trace.
+
+        Parameters
+        ----------
+        funcs : Union[Func_R2_R, list[Func_R2_R]]
+            The functions used to define the trace.
+        """
         if not isinstance(funcs, list):
             if DirichletTrace.check_func(funcs):
                 self.funcs = [funcs for _ in range(self.num_edges)]
@@ -112,8 +158,21 @@ class DirichletTrace:
             self.funcs = funcs
         raise ValueError("'funcs' must be of type Func_R2_R or list[Func_R2_R]")
 
-    @staticmethod
+    @staticmethod  # TODO: Move to a separate module
     def check_func(func: Func_R2_R) -> bool:
+        """
+        Check if a function is a map from R^2 to R.
+
+        Parameters
+        ----------
+        func : Func_R2_R
+            The function to be checked.
+
+        Returns
+        -------
+        bool
+            True if the function is a map from R^2 to R, False otherwise.
+        """
         if not callable(func):
             return False
         sig = inspect.signature(func)
@@ -127,6 +186,14 @@ class DirichletTrace:
     def set_funcs_from_polys(
         self, polys: Union[Polynomial, list[Polynomial]]
     ) -> None:
+        """
+        Set the functions used to define the trace from a list of polynomials.
+
+        Parameters
+        ----------
+        polys : Union[Polynomial, list[Polynomial]]
+            The polynomials used to define the trace.
+        """
         if isinstance(polys, Polynomial):
             self.set_funcs(polys.eval)  # type: ignore
         if not isinstance(polys, list):
@@ -136,14 +203,38 @@ class DirichletTrace:
         self.set_funcs([poly.eval for poly in polys])  # type: ignore
 
     def edges_are_parametrized(self) -> bool:
+        """
+        Check if all edges are parametrized.
+
+        Returns
+        -------
+        bool
+            True if all edges are parametrized, False otherwise.
+        """
         return all(edge.is_parameterized for edge in self.edges)
 
     def find_num_pts(self) -> None:
+        """
+        Find the number of points on the edges.
+
+        Raises
+        ------
+        ValueError
+            If the edges are not parametrized.
+        """
         if not self.edges_are_parametrized():
             raise ValueError("All edges must be parametrized")
         self.num_pts = sum(edge.num_pts for edge in self.edges)
 
     def find_edge_sampled_indices(self) -> None:
+        """
+        Find the start and end indices of the edges in the values array.
+
+        Raises
+        ------
+        ValueError
+            If the edges are not parametrized.
+        """
         if not self.edges_are_parametrized():
             raise ValueError("All edges must be parametrized")
         self.edge_sampled_indices = []
@@ -154,12 +245,23 @@ class DirichletTrace:
             start = end
 
     def find_values(self) -> None:
+        """
+        Find the values of the trace on the edges by evaluating the trace
+        functions at the sampled points on each edge.
+
+        Raises
+        ------
+        ValueError
+            If the functions are not set.
+        ValueError
+            If the edges are not parametrized.
+        """
         if not hasattr(self, "funcs"):
             raise ValueError("The functions must be set before evaluating")
         if not self.edges_are_parametrized():
             raise ValueError("All edges must be parametrized")
         self.find_num_pts()
-        self.find_edge_indices()
+        self.find_edge_sampled_indices()
         self.values = np.zeros(self.num_pts)
         for k, edge in enumerate(self.edges):
             start, end = self.edge_sampled_indices[k]
