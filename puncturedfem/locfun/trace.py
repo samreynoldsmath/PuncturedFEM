c2ce28b7bcc69c9ad7905fff035c2a9df2555e9a
Sam Reynolds <sreyn@proton.me>
2024-03-13 12:42:31 -0700

    add edge_sampled_indices() attribute, ability to set trace functions using a single function

--- puncturedfem/locfun/trace.py
+++ puncturedfem/locfun/trace.py
@@ -39,6 +39,8 @@ class DirichletTrace:
         The values of the trace on the edges.
     funcs : list[Func_R2_R]
         The functions used to define the trace.
+    edge_sampled_indices : list[tuple[int, int]]
+        The start and end indices of the edges in the values array.
 
     Usage
     -----
@@ -46,9 +48,11 @@ class DirichletTrace:
     """
 
     edges: list[Edge]
+    num_edges: int
     num_pts: int
     values: np.ndarray
     funcs: list[Func_R2_R]
+    edge_sampled_indices: list[tuple[int, int]]
 
     def __init__(
         self, edges: Union[MeshCell, list[Edge]], custom: bool = False
@@ -67,6 +71,7 @@ class DirichletTrace:
             if not all(isinstance(edge, Edge) for edge in edges):
                 raise ValueError("All elements must be of type Edge")
             self.edges = edges
+            self.num_edges = len(edges)
         else:
             raise ValueError("'edges' must be of type MeshCell or list[Edge]")
 
@@ -80,13 +85,21 @@ class DirichletTrace:
                 )
             self.values = values
 
-    def set_funcs(self, funcs: list[Func_R2_R]) -> None:
-        for func in funcs:
-            if not DirichletTrace.check_func(func):
-                raise ValueError(
-                    "All elements must be of callable maps from R^2 to R"
-                )
-        self.funcs = funcs
+    def set_funcs(self, funcs: Union[Func_R2_R, list[Func_R2_R]]) -> None:
+        if not isinstance(funcs, list):
+            if DirichletTrace.check_func(funcs):
+                self.funcs = [funcs for _ in range(self.num_edges)]
+                return
+        if isinstance(funcs, list):
+            for func in funcs:
+                if not DirichletTrace.check_func(func):
+                    raise ValueError(
+                        "All elements must be of callable maps from R^2 to R"
+                    )
+            self.funcs = funcs
+        raise ValueError(
+            "'funcs' must be of type Func_R2_R or list[Func_R2_R]"
+        )
 
     @staticmethod
     def check_func(func: Func_R2_R) -> bool:
@@ -100,12 +113,16 @@ class DirichletTrace:
             for param in params
         )
 
-    def set_funcs_from_polys(self, polys: list[Polynomial]) -> None:
+    def set_funcs_from_polys(
+        self, polys: Union[Polynomial, list[Polynomial]]
+    ) -> None:
+        if isinstance(polys, Polynomial):
+            self.set_funcs(polys.eval) # type: ignore
         if not isinstance(polys, list):
             raise ValueError("'polys' must be of type list[Polynomial]")
         if not all(isinstance(poly, Polynomial) for poly in polys):
             raise ValueError("All elements must be of type Polynomial")
-        self.funcs = [poly.eval for poly in polys]  # type: ignore
+        self.set_funcs([poly.eval for poly in polys]) # type: ignore
 
     def edges_are_parametrized(self) -> bool:
         return all(edge.is_parameterized for edge in self.edges)
@@ -115,12 +132,24 @@ class DirichletTrace:
             raise ValueError("All edges must be parametrized")
         self.num_pts = sum(edge.num_pts for edge in self.edges)
 
-    def find_values(self) -> None:
+    def find_edge_sampled_indices(self) -> None:
         if not self.edges_are_parametrized():
             raise ValueError("All edges must be parametrized")
-        self.values = np.zeros(self.num_pts)
+        self.edge_sampled_indices = []
         start = 0
-        for k, edge in enumerate(self.edges):
+        for edge in self.edges:
             end = start + edge.num_pts
-            self.values[start:end] = edge.evaluate_function(self.funcs[k])
+            self.edge_sampled_indices.append((start, end))
             start = end
+
+    def find_values(self) -> None:
+        if not hasattr(self, "funcs"):
+            raise ValueError("The functions must be set before evaluating")
+        if not self.edges_are_parametrized():
+            raise ValueError("All edges must be parametrized")
+        self.find_num_pts()
+        self.find_edge_indices()
+        self.values = np.zeros(self.num_pts)
+        for k, edge in enumerate(self.edges):
+            start, end = self.edge_sampled_indices[k]
+            self.values[start:end] = edge.evaluate_function(self.funcs[k])
