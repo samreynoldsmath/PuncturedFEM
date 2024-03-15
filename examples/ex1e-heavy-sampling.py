d342a66afc395a35433fe8ab4ec6b53a7060410a
Sam Reynolds <sreyn@proton.me>
2024-03-15 12:37:41 -0700

    modify: remove DirichletTrace tutorial from ex1e

--- examples/ex1e-heavy-sampling.py
+++ examples/ex1e-heavy-sampling.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 # # Example 1.E: Heavy Sampling of an Intricate Edge
+# ### Sam Reynolds, 2024
 # 
 # We may sometimes have an edge that has fine details that need to be resolved by increasing the sampling parameter $n$, with the edge being sampled at $2n+1$ points, including the end points.
 # 
@@ -20,8 +21,6 @@ parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
 sys.path.append(parent_dir)
 
 import puncturedfem as pf
-import numpy as np
-import matplotlib.pyplot as plt
 
 # define vertices
 verts: list[pf.Vert] = []
@@ -39,7 +38,7 @@ edges.append(
         pos_cell_idx=0,
         curve_type="sine_wave",
         amp=0.1,
-        freq=8,
+        freq=4,
     )
 )
 edges.append(pf.Edge(verts[1], verts[2], pos_cell_idx=0))
@@ -53,45 +52,33 @@ K_simple = pf.MeshCell(idx=0, edges=edges)
 K_simple.parameterize(quad_dict=pf.get_quad_dict(n=64))
 
 # set up Nystrom solver
-nyst = pf.NystromSolver(K_simple)
+nyst = pf.NystromSolver(K_simple, debug=True)
 
 # plot boundary
 pf.plot.MeshPlot(K_simple.get_edges()).draw()
 
 
-# Notice that the area of the mesh cell $K$ is $|K|=1$.
-# We could compute this by integrating the constant function $v=1$:
+# It is simple to verify that $v\in V_1(K)$ given by $v(x_1,x_2) = x_2$ has a square $L^2$ norm of
 # \begin{align*}
-#     1 = \int_K v^2~dx~.
+#     \int_K v^2 ~dx = \frac13~.
 # \end{align*}
+# Let's verify this:
 
 # In[ ]:
 
 
-A_op = nyst.double_layer_op
-I = np.eye(A_op.shape[0])
-A = np.zeros((A_op.shape[0], A_op.shape[1]))
-for i in range(A_op.shape[0]):
-    A[:, i] = A_op @ I[:, i]
-A += np.ones(A.shape) * 2 * np.pi * K_simple.num_edges / K_simple.num_pts
-print(f"Condition number of A = {np.linalg.cond(A):.2e}")
+# define v to have a Dirichlet trace of x_2 on each edge
+x2 = pf.Polynomial([(1.0, 0, 1)])
+v_trace = pf.PiecewisePolynomial(num_polys=4, polys=[x2, x2, x2, x2])
 
-
-# In[ ]:
-
-
-# define v to have a Dirichlet trace of 1 on each edge
-one = pf.Polynomial([(1.0, 0, 0)])
-v_trace = pf.PiecewisePolynomial(num_polys=4, polys=[one, one, one, one])
-
-# the constant function v = 1 is harmonic
+# the local function v = x_2 is harmonic
 v = pf.LocalFunction(nyst=nyst, lap_poly=pf.Polynomial(), poly_trace=v_trace)
 v.compute_all()
 
 # compute area and error
-area_exact = 1.0
-area_computed = v.get_l2_inner_prod(v)
-print(f"Error in computed area = {np.abs(area_exact - area_computed)}")
+L2_exact = 1 / 3
+L2_computed = v.get_l2_inner_prod(v)
+print(f"Error = {abs(L2_exact - L2_computed):.4e}")
 
 
 # ## When Things Go Wrong
@@ -108,7 +95,7 @@ edges[0] = pf.Edge(
     pos_cell_idx=0,
     curve_type="sine_wave",
     amp=0.1,
-    freq=8,  # this is scary
+    freq=32,  # increase frequency
 )
 
 # define and parameterize a new mesh cell
@@ -143,16 +130,16 @@ print(f"n = {K.num_pts // K.num_edges // 2}")
 
 
 # set up Nystrom solver
-nyst = pf.NystromSolver(K, verbose=True)
+nyst = pf.NystromSolver(K, debug=True)
 
-# the constant function v = 1
+# the harmonic function v = x_2
 v = pf.LocalFunction(nyst=nyst, lap_poly=pf.Polynomial(), poly_trace=v_trace)
 v.compute_all()
 
-# compute area and error
-area_exact = 1.0
-area_computed = v.get_l2_inner_prod(v)
-print(f"Error in computed area = {np.abs(area_exact - area_computed)}")
+# compute square L^2 norm and error
+L2_exact = 1 / 3
+L2_computed = v.get_l2_inner_prod(v)
+print(f"Error = {abs(L2_exact - L2_computed):.4e}")
 
 
 # One might expect that if we increase the sampling parameter, this error will get smaller. 
@@ -164,38 +151,37 @@ print(f"Error in computed area = {np.abs(area_exact - area_computed)}")
 # get 1024 sampled points on each edge
 K.parameterize(quad_dict=pf.get_quad_dict(n=512))
 
-# (WARNING!) this line will result in an exception being thrown
-nyst = pf.NystromSolver(K, verbose=True)
+try:
+    # (WARNING!) this line will result in an exception being thrown
+    nyst = pf.NystromSolver(K, debug=True)
+except ZeroDivisionError as e:
+    print("Indeed, an exception was thrown!\n", e)
 
 
 # ## Changing the Kress parameter (optional)
 # As we saw in [Example 0](ex0-mesh-building.ipynb), we can change the Kress parameter $p$ to adjust how much the sampled points are "clustered" near the endpoints. 
-# The default value is $p=7$, but changing this to its lowest value $p=2$ results in sampled points that are more spread out, perhaps enough so that we can avoid division by machine zero. Let's try it (this may take a while):
+# The default value is $p=7$, but changing this to its lowest value $p=2$ results in sampled points that are more spread out, perhaps enough so that we can avoid division by machine zero.
+# 
+# **NOTE:** The condition number of the Nyström matrix is very high and GMRES will not converge quickly, if at all. Uncomment the following cell to see this.
 
 # In[ ]:
 
 
-# get 1024 sampled points on each edge with lower Kress parameter
-K.parameterize(quad_dict=pf.get_quad_dict(n=512, p=2))
-nyst = pf.NystromSolver(K, verbose=True)
+# # get 1024 sampled points on each edge with lower Kress parameter
+# K.parameterize(quad_dict=pf.get_quad_dict(n=512, p=2))
+# nyst = pf.NystromSolver(K, debug=True)
 
+# # the harmonic function v = x_2
+# v = pf.LocalFunction(nyst=nyst, lap_poly=pf.Polynomial(), poly_trace=v_trace)
 
-# The `NystromSolver` initialized without errors, so let's try to compute our quantity of interest:
+# # (WARNING!) this line will take a long time to run
+# v.compute_all()
 
-# In[ ]:
+# # compute square L^2 norm and error
+# L2_exact = 1 / 3
+# L2_computed = v.get_l2_inner_prod(v)
+# print(f"Error = {abs(L2_exact - L2_computed):.4e}")
 
-
-# the constant function v = 1
-v = pf.LocalFunction(nyst=nyst, lap_poly=pf.Polynomial(), poly_trace=v_trace)
-v.compute_all()
-
-# compute area and error
-area_exact = 1.0
-area_computed = v.get_l2_inner_prod(v)
-print(f"Error in computed area = {np.abs(area_exact - area_computed)}")
-
-
-# This worked well enough, but $10^{-6}$ is kind of a big error for the amount of work we had to do. Let's see if we can do better with a different strategy.
 
 # ## Splitting Edges
 # 
@@ -204,132 +190,52 @@ print(f"Error in computed area = {np.abs(area_exact - area_computed)}")
 # In[ ]:
 
 
-# split edge 0 in half
-e1, e2 = pf.split_edge(e=edges[0], t_split=np.pi)
-
-# split into quarters
-e1_1, e1_2 = pf.split_edge(e1, t_split=np.pi / 2)
-e2_1, e2_2 = pf.split_edge(e2, t_split=3 * np.pi / 2)
-
-# split into eighths
-e1_1_1, e1_1_2 = pf.split_edge(e1_1, t_split=np.pi / 4)
-e1_2_1, e1_2_2 = pf.split_edge(e1_2, t_split=3 * np.pi / 4)
-e2_1_1, e2_1_2 = pf.split_edge(e2_1, t_split=5 * np.pi / 4)
-e2_2_1, e2_2_2 = pf.split_edge(e2_2, t_split=7 * np.pi / 4)
-
 # replace edge 0 with eight new edges
-edges += [e1_1_1, e1_1_2, e1_2_1, e1_2_2, e2_1_1, e2_1_2, e2_2_1, e2_2_2]
+edges += pf.split_edge(edges[0], num_edges=8)
 del edges[0]
 
 # define mesh cell
 K = pf.MeshCell(idx=0, edges=edges)
 
+
+# In the previous section, we tried sampling each edge with $2n = 1024$ points. Notice, though, that only the bottom edge is problematic, and we might get away with sampling the straight edges at a lower rate. To keep the number of sampled points on the bottom edge the same, which has now been split into 8 edges, we need to set the sampling parameter to $n=64=512/8$.
+
+# In[ ]:
+
+
 # bottom edge sampled at 1024 points
 K.parameterize(quad_dict=pf.get_quad_dict(n=64))
 
 # set up Nystrom solver
-nyst = pf.NystromSolver(K, verbose=True)
+nyst = pf.NystromSolver(K, debug=True)
 
 
-# In[ ]:
-
-
-A_op = nyst.double_layer_op
-I = np.eye(A_op.shape[0])
-A = np.zeros((A_op.shape[0], A_op.shape[1]))
-for i in range(A_op.shape[0]):
-    A[:, i] = A_op @ I[:, i]
-A += np.ones(A.shape) * 2 * np.pi * K.num_edges / K.num_pts
-print(f"Condition number of A = {np.linalg.cond(A):.2e}")
-
-
-# The `NystromSolver` didn't crash this time. 
-# Let's see if we can accurately compute the area:
+# The `NystromSolver` didn't crash this time. Let's define the local function $v = x_2$ and take a peek at its trace:
 
 # In[ ]:
 
 
-# Dirichlet trace of constant function v = 1
-v_trace = pf.PiecewisePolynomial(
-    num_polys=K.num_edges, polys=K.num_edges * [one]
-)
+# Dirichlet trace of the harmonic function v = x_2
+x2 = pf.Polynomial([(1.0, 0, 1)])
+v_trace = pf.DirichletTrace(K, funcs=x2)
 
-# constant function v = 1
-v = pf.LocalFunction(nyst=nyst, lap_poly=pf.Polynomial(), poly_trace=v_trace)
-v.compute_all()
+# the harmonic function v = x_2
+v = pf.LocalFunction(nyst=nyst, lap_poly=pf.Polynomial(), has_poly_trace=False)
+v.set_trace_values(v_trace.values) # TODO: pass DirichletTrace object directly to LocalFunction
 
-# compute area and error
-area_exact = 1.0
-area_computed = v.get_l2_inner_prod(v)
-print(f"Error in computed area = {np.abs(area_exact-area_computed)}")
-
-
-# This gave us a much better error than changing the Kress parameter, and the computation was much faster.
-
-# ## Using the DirchletTrace class on MeshCells with Split Edges
-# *Note:* The technique used in this section is likely to be automated in future releases.
-# 
-# Consider the harmonic function $v \in V_1(K)$ with a linear trace 
-# \begin{align*}
-#     v|_{\partial K}(x,y) = y
-# \end{align*}
-# on the bottom edge of the square, and zero on the other three edges.
-# We will use the `DirichletTrace` class to define the corresponding `LocalFunction` object.
-# 
-# The `DirichletTrace` class is initialized with a list of `Edge` objects, or, alternatively, a `MeshCell` object, on which the trace is to be defined.
-# We have several ways to set the trace values. 
-# - We can define a function $f : \mathbb{R}^2 \to \mathbb{R}$ and pass this function to the `set_funcs()` method. **Note:** The function $f$ must be typed, e.g. `def f(x: float, y: float) -> float: return x*y`. Untyped functions will throw an exception. The types can be `float`, `int`, or `numpy.ndarray`.
-# - We can set the trace function on a specific edge with the `set_funcs_on_edge()` method, which accepts the index of the edge (i.e. its position in the list of edges) and a function $f$ as above.
-# - We can set the trace values directly with the `set_values()` method, which accepts a `numpy.ndarray` of shape `(N,1)`, where `N` is the number of points on the edge.  We can set the trace to a constant by passing a `float` or `int` instead of an array.
-# - We can set the trace values on a single edge with the `set_values_on_edge()` method, which accepts a `numpy.ndarray` of shape `(2n,1)`, where `2n` is the number of points on the edge, excluding the terminal point. Again, we can set the trace to a constant by passing a `float` or `int` instead of an array.
-# 
-# Below, we use the `set_funcs()` method to define the same trace function on all the edges.
-
-# In[ ]:
-
-
-v_trace = pf.DirichletTrace(K, custom=True)
-def f(x: float, y: float) -> float:
-    return y
-v_trace.set_funcs(f)
-v_trace.find_values()
-
-
-# We can set the trace values on the three straight edges (which have indices 0, 1,  and 2) to zero by using the `set_values_on_edge()` method.
-
-# In[ ]:
-
-
-for k in [0, 1, 2]:
-    v_trace.set_trace_values_on_edge(k, 0)
-
-
-# Let's take a look at a plot of the trace values:
-
-# In[ ]:
-
-
+# plot the trace of v
+# TODO: TracePlot should be able to initialize its own quad_dict
 pf.plot.TracePlot(v_trace.values, K, quad_dict=pf.get_quad_dict(n=64)).draw()
 
 
-# Let's define the `LocalFunction` object and compute the harmonic conjugate, normal derivative, etc.
+# Finally, let's see if we can accurately compute our quantity of interest:
 
 # In[ ]:
 
 
-v = pf.LocalFunction(nyst=nyst, lap_poly=pf.Polynomial(), has_poly_trace=False)
-v.set_trace_values(v_trace.values)
+# this can take a few minutes
 v.compute_all()
+L2_exact = 1 / 3
+L2_computed = v.get_l2_inner_prod(v)
+print(f"Error = {abs(L2_exact - L2_computed):.4e}")
 
-
-# The `compute_all()` method does not compute interior values, so let's do that now and take a look at the plot:
-
-# In[ ]:
-
-
-v.compute_interior_values()
-pf.plot.LocalFunctionPlot(v).draw()
-
-
-# ## TODO: Creating a LocalFunctionSpace on a MeshCell with a Split Edge
-# This is a [planned feature](../doc/TODO.md).
