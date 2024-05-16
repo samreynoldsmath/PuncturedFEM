7e1b8153a2e40431bc3d97fba6ad9f61eb81d342
Sam Reynolds <sreyn@proton.me>
2024-05-16 14:17:45 -0700

    clean up GlobalFunctionSpace and Solver

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -12,52 +12,51 @@ import puncturedfem as pf
 TOL = 1e-12
 
 
-def build_solver() -> pf.Solver:
-    """
-    Helper function: build a mesh and bilinear form, return a solver object.
-    """
-
+def _build_solver(deg: int) -> pf.Solver:
     # set test parameters
     n = 16
-    deg = 1
     a = 1.0
     c = 1.0
     f = pf.Polynomial([(1.0, 0, 0)])
 
     # define bilinear form
-    B = pf.BilinearForm(
+    bilinear_form = pf.BilinearForm(
         diffusion_constant=a,
         reaction_constant=c,
         rhs_poly=f,
     )
 
     # build mesh
-    T = pf.meshlib.pacman_subdiv(verbose=False)
+    mesh = pf.meshlib.pacman_subdiv(verbose=False)
 
     # build quadrature dictionary for parameterization
     quad_dict = pf.get_quad_dict(n)
 
     # build global function space
-    V = pf.GlobalFunctionSpace(T, deg, quad_dict, verbose=False)
+    glob_fun_sp = pf.GlobalFunctionSpace(mesh, deg, quad_dict, verbose=False)
 
     # return solver object
-    return pf.Solver(V, B)
+    return pf.Solver(
+        glob_fun_sp, bilinear_form, verbose=False, compute_interior_values=False
+    )
 
 
 def test_solver_deg1() -> None:
     """
-    Test the solver class.
+    Test the Solver class with degree 1.
+
+    See Also
+    --------
+    examples/ex2.1-pacman-fem.ipynb
     """
+    solver = _build_solver(deg=1)
+    solver.solve()
 
-    S = build_solver()
-    S.assemble(verbose=False, compute_interior_values=False)
-    S.solve()
-
-    x_computed = S.soln
+    x_computed = solver.soln
     x_exact = np.loadtxt("tests/data/glob_soln_n16.txt")
     x_error = x_exact - x_computed
 
-    h1_error = np.dot(S.stiff_mat @ x_error, x_error)
-    l2_error = np.dot(S.mass_mat @ x_error, x_error)
+    h1_error = np.dot(solver.stiff_mat @ x_error, x_error)
+    l2_error = np.dot(solver.mass_mat @ x_error, x_error)
 
     assert h1_error + l2_error < TOL
