598e096666028d02b1510e7b46f68c9ec8e026b8
Sam Reynolds <sreyn@proton.me>
2024-05-20 12:42:55 -0700

    replace LocalFunction objects with LocalPoissonFunction objects in LocalFunctionSpace

--- puncturedfem/locfun/locfunsp.py
+++ puncturedfem/locfun/locfunsp.py
@@ -8,13 +8,12 @@ LocalFunctionSpace
 
 from typing import Optional
 
-from tqdm import tqdm
-
 from ..mesh.cell import MeshCell
 from ..solver.globkey import GlobalKey
 from .edge_space import EdgeSpace
-from .locfun import LocalFunction, Polynomial
+from .local_poisson import LocalPoissonFunction
 from .nystrom import NystromSolver
+from .poly.poly import Polynomial
 from .trace import DirichletTrace
 
 
@@ -22,15 +21,36 @@ class LocalFunctionSpace:
     """
     A basis of the local Poisson space V_p(K).
 
-    A collection of local functions LocalFunction objects that form a basis of
-    the local Poisson space V_p(K). The LocalFunctions are partitioned into
-    three types:
+    A collection of LocalPoissonFunction objects that form a basis of the local
+    Poisson space V_p(K). The functions are partitioned into three types:
             vert_funs: vertex functions (harmonic, trace supported on two edges)
             edge_funs: Edge functions (harmonic, trace supported on one Edge)
             bubb_funs: bubble functions (Polynomial Laplacian, zero trace)
-    In the case where the mesh cell K has a vertex-free Edge (that is, the Edge
+    In the case where the mesh cell K has a vertex-free edge (that is, the edge
     is a simple closed contour, e.g. a circle), no vertex functions are
-    associated with that Edge.
+    associated with that edge. In this case, the edge functions are the only
+    functions associated with the edge, which are simply traces of polynomials.
+
+    Attributes
+    ----------
+    deg : int
+        Degree of local Poisson space.
+    num_vert_funs : int
+        Number of vertex functions.
+    num_edge_funs : int
+        Number of edge functions.
+    num_bubb_funs : int
+        Number of bubble functions.
+    num_funs : int
+        Total number of functions.
+    vert_funs : list[LocalPoissonFunction]
+        List of vertex functions.
+    edge_funs : list[LocalPoissonFunction]
+        List of edge functions.
+    bubb_funs : list[LocalPoissonFunction]
+        List of bubble functions.
+    nyst : NystromSolver
+        Nystrom solver object for solving integral equations.
     """
 
     deg: int
@@ -38,9 +58,9 @@ class LocalFunctionSpace:
     num_edge_funs: int
     num_bubb_funs: int
     num_funs: int
-    vert_funs: list[LocalFunction]
-    edge_funs: list[LocalFunction]
-    bubb_funs: list[LocalFunction]
+    vert_funs: list[LocalPoissonFunction]
+    edge_funs: list[LocalPoissonFunction]
+    bubb_funs: list[LocalPoissonFunction]
     nyst: NystromSolver
 
     def __init__(
@@ -51,7 +71,6 @@ class LocalFunctionSpace:
         compute_interior_values: bool = True,
         compute_interior_gradient: bool = False,
         verbose: bool = True,
-        processes: int = 1,
     ) -> None:
         """
         Initialize the LocalFunctionSpace object.
@@ -81,25 +100,16 @@ class LocalFunctionSpace:
         # set up Nyström solver
         self.nyst = NystromSolver(K, verbose=verbose)
 
-        # bubble functions: zero trace, Polynomial Laplacian
-        self.build_bubble_funs()
-
-        # build vertex functions...')
-        self.build_vert_funs(edge_spaces)
-
-        # build Edge functions
-        self.build_edge_funs(edge_spaces)
-
-        # count number of each type of function
-        self.compute_num_funs()
-
-        # compute all function metadata
-        self.compute_all(verbose=verbose, processes=processes)
+        # build each type of function
+        self._build_bubb_funs()
+        self._build_vert_funs(edge_spaces)
+        self._build_edge_funs(edge_spaces)
+        self._compute_num_funs()
 
         # find interior values
         if compute_interior_values or compute_interior_gradient:
-            self.find_interior_values(
-                verbose=verbose, compute_grad=compute_interior_gradient
+            raise NotImplementedError(
+                "Interior values and gradients are not yet implemented"
             )
 
     def set_deg(self, deg: int) -> None:
@@ -117,39 +127,18 @@ class LocalFunctionSpace:
             raise ValueError("deg must be a positive integer")
         self.deg = deg
 
-    def find_interior_values(
-        self, verbose: bool = True, compute_grad: bool = False
-    ) -> None:
+    def get_basis(self) -> list[LocalPoissonFunction]:
         """
-        Compute interior values for all basis functions.
+        Get the list of all basis functions.
 
-        Parameters
-        ----------
-        verbose : bool, optional
-            Print progress, by default True
-        compute_grad : bool, optional
-            Compute gradient of basis functions, by default False
+        Returns
+        -------
+        list[LocalPoissonFunction]
+            List of all basis functions.
         """
-        if verbose:
-            print("Finding interior values...")
-            for v in tqdm(self.get_basis()):
-                v.compute_interior_values(compute_grad)
-        else:
-            for v in self.get_basis():
-                v.compute_interior_values(compute_grad)
+        return self.vert_funs + self.edge_funs + self.bubb_funs
 
-    # BUILD FUNCTIONS ########################################################
-
-    def compute_num_funs(self) -> None:
-        """
-        Sum the number of vertex, edge, and bubble functions.
-
-        Sets the following attributes:
-            num_vert_funs: number of vertex functions
-            num_edge_funs: number of edge functions
-            num_bubb_funs: number of bubble functions
-            num_funs: total number of functions
-        """
+    def _compute_num_funs(self) -> None:
         self.num_vert_funs = len(self.vert_funs)
         self.num_edge_funs = len(self.edge_funs)
         self.num_bubb_funs = len(self.bubb_funs)
@@ -157,50 +146,11 @@ class LocalFunctionSpace:
             self.num_vert_funs + self.num_edge_funs + self.num_bubb_funs
         )
 
-    def compute_all(self, verbose: bool = True, processes: int = 1) -> None:
-        """
-        Equivalent to running v.compute_all(K) for eachLocalFunction v.
-
-        Parameters
-        ----------
-        verbose : bool, optional
-            Print progress, by default True.
-        processes : int, optional
-            Number of processes to use for parallel computation, by default 1.
-        """
-        if processes == 1:
-            self._compute_all_sequential(verbose=verbose)
-        elif processes > 1:
-            self._compute_all_parallel(verbose=verbose, processes=processes)
-        else:
-            raise ValueError("processes must be a positive integer")
-
-    def _compute_all_sequential(self, verbose: bool = True) -> None:
-        # if verbose:
-        #     print("Computing function metadata...")
-        #     for v in tqdm(self.get_basis()):
-        #         v.compute_all()
-        # else:
-        #     for v in self.get_basis():
-        #         v.compute_all()
-        pass
-
-    def _compute_all_parallel(
-        self, verbose: bool = True, processes: int = 1
-    ) -> None:
-        raise NotImplementedError("Parallel computation not yet implemented")
-
-    def build_bubble_funs(self) -> None:
-        """
-        Construct bubble functions.
-
-        Bubble functions are zero on the boundary and have a Polynomial
-        Laplacian.
-
-        Sets the following attribute:
-            bubb_funs: list of bubble functions
-        """
-        # bubble functions
+    def _build_bubb_funs(self) -> None:
+        # Bubble functions are zero on the boundary and have a polynomial
+        # Laplacian.
+        if self.deg < 2:
+            return
         num_bubb = (self.deg * (self.deg - 1)) // 2
         self.bubb_funs = []
         for k in range(num_bubb):
@@ -208,28 +158,13 @@ class LocalFunctionSpace:
             p = Polynomial()
             p.add_monomial_with_idx(coef=-1.0, idx=k)
             self.bubb_funs.append(
-                LocalFunction(nyst=self.nyst, laplacian=p, key=v_key)
+                LocalPoissonFunction(nyst=self.nyst, laplacian=p, key=v_key)
             )
 
-    def build_vert_funs(self, edge_spaces: list[EdgeSpace]) -> None:
-        """
-        Construct vertex functions from edge spaces.
-
-        Vertex functions are harmonic and have trace supported on two edges,
-        with the common vertex having a value of 1 and all other vertices having
-        a value of 0.
-
-        Parameters
-        ----------
-        edge_spaces : list[EdgeSpace]
-            List of EdgeSpace objects for each Edge in K.
-
-        Notes
-        -----
-        Sets the following attribute:
-            vert_funs: list of vertex functions.
-        """
-        # find all Vertices on MeshCell
+    def _build_vert_funs(self, edge_spaces: list[EdgeSpace]) -> None:
+        # Vertex functions are harmonic and have trace supported on two edges,
+        # with the common vertex having a value of 1 and all other vertices having
+        # a value of 0.
         vert_idx_set = set()
         for c in self.nyst.K.components:
             for e in c.edges:
@@ -243,59 +178,39 @@ class LocalFunctionSpace:
         # initialize list of vertex functions and set traces
         self.vert_funs = []
         for vert_key in vert_keys:
-            polys = [Polynomial() for _ in range(self.nyst.K.num_edges)]
+            v_trace = DirichletTrace(
+                edges=self.nyst.K.get_edges(), funcs=lambda x, y: 0
+            )
             for j, b in enumerate(edge_spaces):
                 for k in range(b.num_vert_funs):
                     if b.vert_fun_global_keys[k].vert_idx == vert_key.vert_idx:
-                        polys[j] = b.vert_fun_traces[k]
-            v_trace = DirichletTrace(
-                edges=self.nyst.K.get_edges(),
-                funcs=polys,
+                        v_trace.set_func_from_poly_on_edge(
+                            edge_index=j, poly=b.vert_fun_traces[k]
+                        )
+            v = LocalPoissonFunction(
+                nyst=self.nyst, trace=v_trace, key=vert_key
             )
-            v = LocalFunction(nyst=self.nyst, trace=v_trace, key=vert_key)
             self.vert_funs.append(v)
 
-    def build_edge_funs(self, edge_spaces: list[EdgeSpace]) -> None:
-        """
-        Construct edge functions from edge spaces.
-
-        Edge functions are harmonic and have trace supported on one edge.
-
-        Parameters
-        ----------
-        edge_spaces : list[EdgeSpace]
-            List of EdgeSpace objects for each Edge in K.
-        """
-        # initialize list of Edge functions
+    def _build_edge_funs(self, edge_spaces: list[EdgeSpace]) -> None:
+        # Edge functions are harmonic and have trace supported on one edge.
         self.edge_funs = []
-
-        # loop over edges on MeshCell
         for b in edge_spaces:
             # locate Edge within MeshCell
             glob_edge_idx = b.e.idx
             glob_edge_idx_list = [e.idx for e in self.nyst.K.get_edges()]
             edge_idx = glob_edge_idx_list.index(glob_edge_idx)
 
-            # loop over Edge functions
+            # loop over edge functions
             for k in range(b.num_edge_funs):
-                polys = [Polynomial() for _ in range(self.nyst.K.num_edges)]
-                polys[edge_idx] = b.edge_fun_traces[k]
                 v_trace = DirichletTrace(
                     edges=self.nyst.K.get_edges(),
-                    funcs=polys,
+                    funcs=lambda x, y: 0,
                 )
-                v = LocalFunction(
+                v_trace.set_func_from_poly_on_edge(
+                    edge_index=edge_idx, poly=b.edge_fun_traces[k]
+                )
+                v = LocalPoissonFunction(
                     nyst=self.nyst, trace=v_trace, key=b.edge_fun_global_keys[k]
                 )
                 self.edge_funs.append(v)
-
-    def get_basis(self) -> list[LocalFunction]:
-        """
-        Get the list of all basis functions.
-
-        Returns
-        -------
-        list[LocalFunction]
-            List of all basis functions.
-        """
-        return self.vert_funs + self.edge_funs + self.bubb_funs
